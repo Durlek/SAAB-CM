36779da8192760e3982d271d9f6e9ad46bdc90d0
GULPF <oscarnihlgard@gmail.com>
2015-11-19 00:51:51 +0100

    AP2Ce Datakontrakt

---
+++ Drivers/Saab/Saab.CBRN.Wcf/DataContracts/AP2CeData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Saab.CBRN.Wcf.DataContracts
+{
+    class AP2CeData
+    {
+        private int _barCount;
+        private double _volumeConcentration;
+
+        [DataMember]
+        public int BarCount
+        {
+            get { return _barCount; }
+            set { _barCount = value; }
+        }
+
+        [DataMember]
+        public double VolumeConcentration
+        {
+            get { return _volumeConcentration; }
+            set { _volumeConcentration = value;  }
+        }
+    }
+}
